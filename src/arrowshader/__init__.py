fc24aa626c56be2cdc70f40ee2f60122004a158b
Christopher Akiki <christopher.akiki@gmail.com>
2026-06-09 17:59:49 +0200

    Add init

---
+++ src/arrowshader/__init__.py
@@ -0,0 +1,4 @@
+from arrowshader.canvas import Canvas
+from arrowshader.reductions import count, sum, mean, by, min, max, std, var, first, last, any
+from arrowshader.data import from_parquet
+from arrowshader.transfer_functions import eq_hist
